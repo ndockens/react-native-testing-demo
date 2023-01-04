ec89a9e6664485c3e878c06e1fc2f53a2fa17004
Nathan Dockens <ndockens92@gmail.com>
2023-01-04 14:54:31 -0600

    Add styling to login screen

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
 import React, { useState } from 'react';
 import {
   Button,
+  Pressable,
   SafeAreaView,
+  StyleSheet,
   Text,
+  TextInput,
   View,
 } from 'react-native';
 
 const LoginScreen = (props) => {
   return (
-    <View testID='login_screen'>
+    <View style={styles.loginScreen} testID='login_screen'>
       <Text>Login</Text>
-      <Button testID='login_button' title='Log In' onPress={() => props.onLogIn({ username: 'joe' })} />
+      <TextInput style={styles.textInput} placeholder='Username' />
+      <TextInput style={styles.textInput} placeholder='Password' />
+      {/* <Button
+        testID='login_button'
+        title='Log In'
+        color={styles.primaryButton.color}
+        onPress={() => props.onLogIn({ username: 'joe' })}
+        /> */}
+      <Pressable
+        onPress={() => props.onLogIn({ username: 'joe' })}
+        style={styles.primaryButton}
+        testID='login_button'>
+        <Text style={styles.primaryButtonText}>Log In</Text>
+      </Pressable>
     </View>
   );
 };
@@ -36,5 +52,33 @@ const App = () => {
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  loginScreen: {
+    padding: 20,
+  },
+  primaryButton: {
+    backgroundColor: 'darkred',
+    borderRadius: 5,
+    height: 50,
+    justifyContent: 'center',
+    marginTop: 30,
+  },
+  primaryButtonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+  textInput: {
+    height: 50,
+    borderRadius: 5,
+    borderWidth: 1,
+    fontSize: 20,
+    marginVertical: 6,
+    padding: 10,
+  }
+});
 
 export default App;
